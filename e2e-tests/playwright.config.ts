3b6f5005cefa13e9c899bc59f83e745cfb43003a
Manas Doshi <manas.d@perfios.com>
2025-07-13 02:31:50 +0530

    skip build in workflow

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -15,11 +15,6 @@ export default defineConfig({
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
-    },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+    }
   ],
 });
