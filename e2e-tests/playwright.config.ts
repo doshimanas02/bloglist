3a659883a5194501a0ddcb455ff0c6641267de99
Manas Doshi <manas.d@perfios.com>
2025-07-13 11:17:22 +0530

    fix url

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: 1,
   reporter: "list",
   use: {
-    baseURL: "http://localhost:8080",
+    baseURL: "http://127.0.0.1:8080",
     trace: "on-first-retry",
   },
   projects: [
