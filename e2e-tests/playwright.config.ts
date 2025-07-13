acbe53e3b08d72b54dea751ca966d498baaf744f
Manas Doshi <manas.d@perfios.com>
2025-07-13 11:58:34 +0530

    try changing port

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: 1,
   reporter: "list",
   use: {
-    baseURL: "http://127.0.0.1:8080",
+    baseURL: "http://127.0.0.1:8123",
     trace: "on-first-retry",
   },
   projects: [
